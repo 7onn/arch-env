7e3d2f1f291c1fa4b41bffab09fa391e01365d90
Tom M G <devbytom@gmail.com>
2022-02-11 18:27:20 -0300

    Set blackout background

--- roles/i3/files/home/.screenlayout/1-screen.sh
+++ roles/i3/files/home/.screenlayout/1-screen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
 sleep 1s
-nitrogen --head=0 --set-zoom-fill /usr/share/backgrounds/purple_cannabis_leaf.jpg
+nitrogen --head=0 --set-zoom-fill /usr/share/backgrounds/blackout.jpg
--- roles/i3/files/home/.screenlayout/3-screens.sh
+++ roles/i3/files/home/.screenlayout/3-screens.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 3000x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate right --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 1080x0 --rotate normal --output VIRTUAL1 --off
 sleep 1s
-nitrogen --head=0 --set-zoom-fill /usr/share/backgrounds/purple_cannabis_leaf.jpg
-nitrogen --head=1 --set-zoom-fill /usr/share/backgrounds/purple_cannabis_leaf.jpg
-nitrogen --head=2 --set-zoom-fill /usr/share/backgrounds/purple_cannabis_leaf.jpg
+nitrogen --head=0 --set-zoom-fill /usr/share/backgrounds/blackout.jpg
+nitrogen --head=1 --set-zoom-fill /usr/share/backgrounds/blackout.jpg
+nitrogen --head=2 --set-zoom-fill /usr/share/backgrounds/blackout.jpg
